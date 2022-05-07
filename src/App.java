6234cfa8ff2f233bae69853121038af549805bde
17acres <dan@17acr.com>
2022-05-07 01:07:20 -0400

    think it works?

---
+++ src/App.java
@@ -0,0 +1,231 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Map.Entry;
+
+public final class App {
+    private App() {
+    }
+    private static TreeMap<Integer,Ballot> ballots=  new TreeMap<>();
+    public static TreeMap<String,TreeMap<String,ArrayList<Ballot>>> allVotes = new TreeMap<>();//first string is election, second is candidate
+    public static ArrayList<String> importantElections = new ArrayList<>();
+    public static TreeMap<String,String> winners = new TreeMap<>();
+    public static String[] columnHeadings;
+    public static PrintWriter writer;
+    public static void main(String[] args) throws IOException {
+        System.out.println("Looking in " + System.getProperty("user.dir")+" for voteTallies.csv");
+        BufferedReader reader;
+        
+        try {
+            reader = new BufferedReader(new FileReader("voteTallies.csv"));
+            writer = new PrintWriter(new FileWriter("voteResults.txt"),true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        String line;
+        int lineIdx=1;
+        while((line = reader.readLine())!=null){
+            line=line.replace("\"","");//google sheets adds a bunch of random quotes
+
+            if(line.contains("Timestamp")){
+                writer.println("Processing Headings");
+                columnHeadings = line.split(",");
+                for (String heading : Arrays.asList(columnHeadings)) {
+                    if(heading.contains("Timestamp")){
+                        continue;
+                    }
+                    String position = getVoteTypeFromHeading(heading).getKey();//remove first/second/third
+                    writer.println("Found column "+heading);
+
+                    if(!allVotes.containsKey(position)){
+                        allVotes.put(position, new TreeMap<>());
+                        writer.println("Created election "+position);
+                        if(position.contains("Captain")){
+                            importantElections.add(position);
+                            writer.println("Important position "+position+" noted");
+                        }
+                    }
+                }
+                writer.println("Elections: "+allVotes.keySet().toString());
+                writer.println("Important elections: "+importantElections.toString());
+            }
+            else{
+                ballots.put(lineIdx, new Ballot(line,lineIdx));
+                writer.println("Found ballot "+line+". Choices: "+ballots.get(lineIdx).toString());
+                lineIdx++;
+            }
+            
+        }
+
+        for(String importantName:importantElections){
+            runElection(importantName);
+        }
+
+        for(Map.Entry<String,TreeMap<String,ArrayList<Ballot>>> elections:allVotes.entrySet()){
+            if(!importantElections.contains(elections.getKey()))
+                runElection(elections.getKey());
+        }
+        
+        writer.println("Results:");
+        winners.forEach((election,winner)->{
+            writer.println("    "+election+": "+winner);
+        });
+
+
+        writer.flush();
+    }   
+    public static SimpleEntry<String,Integer> getVoteTypeFromHeading(String heading){
+        String electionName = heading.replaceAll(" \\[.*", "");//remove first/second/third
+        int precedence=0;
+        if(heading.contains("First")){
+            precedence=0;
+        }else if(heading.contains("Second")){
+            precedence=1;
+        }else if(heading.contains("Third")){
+            precedence=2;
+        }else{
+            System.err.println("Invalid precedence. Exiting");
+            System.exit(-2);
+        }
+        return(new SimpleEntry<String,Integer>(electionName,precedence));
+    }
+
+    public static void runElection(String election) throws IOException{
+        writer.println("Running election "+election);
+
+        ballots.forEach((idx,ballot)->{try {
+            ballot.cast(election);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }});
+        writer.println("Initial casting complete");
+        printElectionStandings(election);
+
+        for(int round=0;round<=10;round++){
+            int numVotes=getNumberOfVotesInElection(election);
+            SimpleEntry<String, Integer> currentLeader=getCurrentLeader(election);
+            if(currentLeader.getValue()>numVotes/2 && currentLeader.getKey()!=null){
+                writer.println("Winner found. Winner is "+currentLeader.getKey()+" with "+currentLeader.getValue()+" votes.");
+                winners.put(election, currentLeader.getKey());
+                break;
+            }
+            int numberOfCandidatesBeforeRemoval=allVotes.get(election).size();
+            SimpleEntry<String, Integer> currentLoser=getCurrentLoser(election);
+            if(currentLoser.getKey()==null){
+                writer.println("Unbroken tie for the lose. See standings and resolve manually.");
+                break;
+            }
+            ArrayList<Ballot> votesForLoser = allVotes.get(election).get(currentLoser.getKey());
+            allVotes.get(election).remove(currentLoser.getKey());
+            writer.println("Candidate "+currentLoser.getKey()+" removed.");
+
+            for(Ballot ballotForLoser:votesForLoser){ //now they are eliminated remove those that voted for them
+                ballotForLoser.cast(election);
+            }
+
+            if(allVotes.get(election).size()>=numberOfCandidatesBeforeRemoval){
+                System.err.println("Recast all votes for "+currentLoser+" but now there are more candidates");
+                System.exit(-5);
+            }
+            writer.println("Starting round "+(round+2));
+            printElectionStandings(election);
+        }
+
+        writer.println("Finished election "+election);
+    }
+
+
+    public static ArrayList<Map.Entry<String,ArrayList<Ballot>>> getSortedStandings(String election){
+        ArrayList<Map.Entry<String,ArrayList<Ballot>>> currentStandings=new ArrayList<>(allVotes.get(election).entrySet());
+        currentStandings.sort(Entry.comparingByValue((ArrayList<Ballot> o1, ArrayList<Ballot> o2)-> {
+                return (int)Math.signum(o2.size()-o1.size());
+        }));
+        return currentStandings;
+    }
+
+    public static void printElectionStandings(String election) {
+        writer.println("Standings of election "+election);
+        for(Map.Entry<String,ArrayList<Ballot>> candidate:getSortedStandings(election)){
+            writer.println("    "+candidate.getValue().size()+": "+candidate.getKey());
+        }
+    }
+
+    public static int getNumberOfVotesInElection(String election) {
+        TreeMap<String,ArrayList<Ballot>> votes = allVotes.get(election);
+        int numTotalVotes=0;
+        for (Map.Entry<String,ArrayList<Ballot>> candidates : votes.entrySet()) {
+            numTotalVotes+=candidates.getValue().size(); 
+        }
+        writer.println(numTotalVotes+" votes in election "+election);
+        return numTotalVotes;
+    }
+
+    public static SimpleEntry<String,Integer> getCurrentLeader(String election){
+        TreeMap<String,ArrayList<Ballot>> votes = allVotes.get(election);
+        String potentialCurrentLeader=null;
+        Integer potentialLeaderVotes=0;
+        for (Map.Entry<String,ArrayList<Ballot>> candidates : votes.entrySet()) {
+            Integer votesForThisCandidate=candidates.getValue().size();
+            if(votesForThisCandidate.equals(potentialLeaderVotes)){
+                potentialCurrentLeader=null;//tied in the lead
+            }else if(votesForThisCandidate>potentialLeaderVotes){
+                potentialCurrentLeader=candidates.getKey();
+                potentialLeaderVotes=votesForThisCandidate;
+            }
+        }
+        return new SimpleEntry<>(potentialCurrentLeader,potentialLeaderVotes);
+    }
+    public static int getNextWorstNumberOfVotes(String election){
+        ArrayList<Map.Entry<String,ArrayList<Ballot>>> currentStandings=getSortedStandings(election);
+        int minVotes=currentStandings.get(currentStandings.size()-1).getValue().size();
+        if(currentStandings.size()<2){
+            return 1;
+        }
+        for(int i=currentStandings.size()-2;i<=0;i++){
+            int thisNumVotes=currentStandings.get(currentStandings.size()-1).getValue().size();
+            if(thisNumVotes>minVotes){
+                return thisNumVotes;
+            }
+        }
+        return 1;
+    }
+    public static SimpleEntry<String,Integer> getCurrentLoser(String election) {
+        TreeMap<String,ArrayList<Ballot>> votes = allVotes.get(election);
+        String potentialCurrentLoser=null;
+        Integer potentialLoserVotes=9999;
+        boolean tie=false;
+        for (Map.Entry<String,ArrayList<Ballot>> candidates : votes.entrySet()) {
+            Integer votesForThisCandidate=candidates.getValue().size();
+            if(votesForThisCandidate.equals(potentialLoserVotes)){
+                tie=true;
+                //tied in the loss, the first randomly chosen person is still the loser
+            }else if(votesForThisCandidate<potentialLoserVotes){
+                potentialCurrentLoser=candidates.getKey();
+                potentialLoserVotes=votesForThisCandidate;
+                tie=false;
+            }
+        }
+        if(tie){
+            if(potentialLoserVotes*2>getNextWorstNumberOfVotes(election)){
+                potentialCurrentLoser=null;//no one can be eliminated simply
+            }else{
+                writer.println("Two candidates tied in the loss of election "+election+" but neither are important. Candidate "+potentialCurrentLoser+" will be eliminated.");
+            }
+        }
+            
+
+        return new SimpleEntry<>(potentialCurrentLoser,potentialLoserVotes);
+    }
+
+
+}
---
+++ src/Ballot.java
@@ -0,0 +1,100 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.AbstractMap.SimpleEntry;
+
+public final class Ballot {
+    private TreeMap<String, ArrayList<String>> votes = new TreeMap<>();
+    private int ballotIndex;
+    private TreeMap<String, Integer> currentChoiceIdxs = new TreeMap<>();
+
+    public Ballot(String ballotLine, int idx) {
+        ballotIndex = idx;
+        String[] voteNames = ballotLine.split(",");
+        if (voteNames.length != App.columnHeadings.length) {
+            System.err.println("Length of ballot " + ballotLine + " doesn't match number of elections!");
+            System.exit(-1);
+        }
+        for (int i = 1; i < voteNames.length; i++) {// start at 1 to skip timestamp
+            String thisColumString = App.columnHeadings[i];
+            SimpleEntry<String, Integer> thisVoteType = App.getVoteTypeFromHeading(thisColumString);
+            String thisElection = thisVoteType.getKey();
+            String thisVote = voteNames[i];
+
+            TreeMap<String, ArrayList<Ballot>> candidatesForThisElection = App.allVotes.get(thisElection);
+            if (!candidatesForThisElection.containsKey(thisVote)) {// candidate not in list for this election
+                candidatesForThisElection.put(thisVote, new ArrayList<>());
+            }
+
+            if (!votes.containsKey(thisElection)) {// no votes by this ballot for this election
+                votes.put(thisElection, new ArrayList<>());
+            }
+
+            ArrayList<String> thisBallotVotesForThisElection = votes.get(thisElection);
+            thisBallotVotesForThisElection.add(thisVote);// better be in order
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder rBuilder = new StringBuilder();
+        for (Map.Entry<String, ArrayList<String>> thisElectionChoices : votes.entrySet()) {
+            rBuilder.append("Election " + thisElectionChoices.getKey() + ":");
+            for (int i = 0; i < thisElectionChoices.getValue().size(); i++) {
+                rBuilder.append(" #" + i + "," + thisElectionChoices.getValue().get(i) + "; ");
+            }
+        }
+        return (rBuilder.toString());
+    }
+
+    public boolean cast(String election) throws IOException {
+        if (!currentChoiceIdxs.containsKey(election)) {
+            currentChoiceIdxs.put(election, -1);
+        }
+        int choice = currentChoiceIdxs.get(election) + 1;
+
+        if (votes.get(election).size() <= choice) {
+            App.writer.println(
+                    "Ballot " + ballotIndex + " thrown away for election " + election + ". Choice " + (choice + 1)
+                            + " not available.");
+            return false;
+        }
+
+        String thisBallotsChoice = votes.get(election).get(choice);
+
+        if (!App.allVotes.get(election).containsKey(thisBallotsChoice)) {
+            votes.get(election).remove(choice);
+            App.writer.println(
+                    "Ballot " + ballotIndex + " choice #" + (choice + 1)
+                            + " was already eliminated. Moving choices up 1.");
+            return cast(election); // now the choice should be valid
+        }
+
+        ArrayList<Ballot> thisCandidatesBallotList = App.allVotes.get(election).get(thisBallotsChoice);
+
+        for (String importantElection : App.importantElections) {
+            if (App.winners.containsKey(importantElection)) {
+                if (App.winners.get(importantElection).equals(thisBallotsChoice)) {
+                    // this ballot's choice is for someone who already won captain
+                    votes.get(election).remove(choice);
+                    App.writer.println(
+                            "Ballot " + ballotIndex + " choice #" + (choice + 1) + " is captain. Moving choices up 1.");
+                    return cast(election); // now the choice should be valid
+                }
+            }
+        }
+
+        if (thisCandidatesBallotList.contains(this)) {
+            System.err.println(
+                    "Ballot " + ballotIndex + " already voted for " + thisBallotsChoice + " in election " + election);
+            System.exit(-2);
+        }
+        thisCandidatesBallotList.add(this);
+        App.writer.println("Ballot " + ballotIndex + " cast for " + thisBallotsChoice + ", choice #" + (choice + 1)
+                + " in election " + election);
+        currentChoiceIdxs.put(election, choice);
+
+        return true;
+    }
+}
